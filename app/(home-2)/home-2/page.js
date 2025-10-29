d4966c010d91f5e7840ea7dc84e8809c8fd8446f
Mian Muhammad Moiz <164487066+moizxox@users.noreply.github.com>
2025-10-29 11:55:26 +0500

    Feature section added

---
+++ app/(home-2)/home-2/page.js
@@ -0,0 +1,17 @@
+'use client';
+
+import Features from '@/app/(components)/home-2/sections/Features';
+import Hero from '@/app/(components)/home-2/sections/Hero';
+import React from 'react';
+
+const Page = () => {
+  return (
+   <>
+   <Hero/>
+   <Features/>
+   </>
+  );
+};
+
+export default Page;
+
